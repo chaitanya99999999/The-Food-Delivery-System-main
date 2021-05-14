feb3e4d7c5f799282d88174eddeadc938ccb8b69
Chaitanya1603 <56607472+chaitanya99999999@users.noreply.github.com>
2021-05-14 22:36:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# The-Food-Delivery-System
+ 
---
+++ food_delivery/conn.php
@@ -0,0 +1,11 @@
+<?php
+$host='localhost';
+ $user='root';
+ $pass='';
+                      //$db='data_a';
+$db='mysql';
+if(!($conn=mysqli_connect($host,$user,$pass))||!(mysqli_select_db($conn,$db)))
+{
+	die("Error");
+}
+?>
